a56391f9e4ba750b25fe17941708f173001e5a34
kyh1126 <kyh901126@nate.com>
2021-05-17 15:38:47 +0900

    [3장. 모두를 위한 평등] 조금 수정해서 테스트가 성공하는 것을 확인한 버전의 Dollar 클래스 (주기 3, 4)

--- src/main/java/money/domain/Dollar.java
+++ src/main/java/money/domain/Dollar.java
@@ -10,4 +10,9 @@ public class Dollar {
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        return true;
+    }
 }
