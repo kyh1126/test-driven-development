782972a2fed28daad34adb4e57902b841fd10151
kyh1126 <kyh901126@nate.com>
2021-05-17 15:01:06 +0900

    [2장. 타락한 객체] 작은 테스트를 하나 추가한다. 모든 테스트를 실행해서 테스트가 실패하는 것을 확인한다. (주기 1, 2)

--- src/test/java/_1_money/DollarTest.java
+++ src/test/java/_1_money/DollarTest.java
@@ -8,9 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DollarTest {
 
     @Test
-    void testMultiplication() {
+    void testMultiplication_1() {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
     }
+
+    @Test
+    void testMultiplication_2() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
 }
