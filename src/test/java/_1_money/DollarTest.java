569c54c31e9c40406585b536a1c5a58078d7d579
kyh1126 <kyh901126@nate.com>
2021-05-17 16:32:23 +0900

    [5장. 솔직히 말하자면] Dollar 테스트를 복사한 Franc 테스트 추가 (주기 1)

--- src/test/java/_1_money/DollarTest.java
+++ src/test/java/_1_money/DollarTest.java
@@ -39,4 +39,11 @@ class DollarTest {
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
+
+    @Test
+    void testMultiplication_5() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
 }
