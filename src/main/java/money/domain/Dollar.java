4e66320baaa2a380d83e0c4ff15513f58a5a3a1a
kyh1126 <kyh901126@nate.com>
2021-05-17 14:12:12 +0900

    [1장. 다중 통화를 지원하는 Money 객체] 컴파일 에러 해결한 버전의 Dollar 클래스

---
+++ src/main/java/money/domain/Dollar.java
@@ -0,0 +1,11 @@
+package money.domain;
+
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+    }
+
+    public void times(int multiplier) {
+    }
+}
--- src/test/java/_1_DollarTest.java
+++
@@ -1,7 +0,0 @@
-public class _1_DollarTest {
-    public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-    }
-}
