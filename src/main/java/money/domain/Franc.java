b6a9d17dfc76a9083391a1abce1aeb6afdf990c1
kyh1126 <kyh901126@nate.com>
2021-05-20 18:46:21 +0900

    [10장. 흥미로운 시간] 작은 시작, times()를 완전히 동일하게 만들도록 팩토리 메서드를 인라인 시킨다.

--- src/main/java/money/domain/Franc.java
+++ src/main/java/money/domain/Franc.java
@@ -6,6 +6,6 @@ public class Franc extends Money {
     }
 
     public Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
+        return new Franc(amount * multiplier, "CHF");
     }
 }
