c6077479ed06e9308afb7fea7fb93fe7c2b04b86
kyh1126 <kyh901126@nate.com>
2021-05-20 20:27:36 +0900

    [11장. 모든 악의 근원] 과한 테스트 코드를 정리한다.

--- src/test/java/_1_money/DollarTest.java
+++ src/test/java/_1_money/DollarTest.java
@@ -82,8 +82,6 @@ class DollarTest {
     void testEquality_8() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
