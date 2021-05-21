2a735f3dfa70437ebe506c0f5fc0079e39e2f566
kyh1126 <kyh901126@nate.com>
2021-05-21 21:45:55 +0900

    [13장. 진짜로 만들기] 작은 테스트(Bank.reduce(Money))를 추가한다. 모든 테스트를 실행해서 테스트가 실패하는 것을 확인한다. 조금 수정한다. Bank.reduce(Money) 일 경우를 고려한다. 모든 테스트를 실행해서 테스트가 성공하는 것을 확인한다. (주기 1, 2, 3, 4)

--- src/main/java/money/domain/Bank.java
+++ src/main/java/money/domain/Bank.java
@@ -5,6 +5,7 @@ public class Bank {
     }
 
     public Money reduce(Expression source, String currency) {
+        if (source instanceof Money) return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(currency);
     }
--- src/test/java/_1_money/DollarTest.java
+++ src/test/java/_1_money/DollarTest.java
@@ -126,4 +126,11 @@ class DollarTest {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(7), result);
     }
+
+    @Test
+    void testReduceMoney_13() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
