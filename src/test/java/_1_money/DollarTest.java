192afde86619089a21df8aee2acb9bad3c02d0b3
kyh1126 <kyh901126@nate.com>
2021-05-23 19:39:01 +0900

    [16장. 드디어, 추상화] 작은 테스트를 추가한다. (주기 1) - Sum.times()에 대한 테스트 추가

--- src/test/java/_1_money/DollarTest.java
+++ src/test/java/_1_money/DollarTest.java
@@ -174,4 +174,16 @@ class DollarTest {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(15), result);
     }
+
+    @Test
+    void testSumTimes_16() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+
+        Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(20), result);
+    }
 }
