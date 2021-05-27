a71c23c54bf787135d9ab29d407ad395500aecb2
kyh1126 <kyh901126@nate.com>
2021-05-28 01:13:36 +0900

    [부록 B. 피보나치] 작은 테스트를 추가한다. 모든 테스트를 실행해서 테스트가 실패하는 것을 확인한다. 조금 수정한다. 모든 테스트를 실행해서 테스트가 성공하는 것을 확인한다. (주기 1, 2, 3, 4)

--- src/test/java/fibonacci/FibonacciTest.java
+++ src/test/java/fibonacci/FibonacciTest.java
@@ -10,10 +10,13 @@ class FibonacciTest {
     void testFibonacci() {
         assertEquals(fib(0), 0);
         assertEquals(fib(1), 1);
+        assertEquals(fib(2), 1);
+        assertEquals(fib(3), 2);
     }
 
     private int fib(int n) {
         if (n == 0) return 0;
-        return 1;
+        if (n <= 2) return 1;
+        return 2;
     }
 }
