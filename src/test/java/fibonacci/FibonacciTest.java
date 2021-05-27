2204c582fa2f829de86f1a722b425bed4fc6548b
kyh1126 <kyh901126@nate.com>
2021-05-28 01:19:15 +0900

    [부록 B. 피보나치] 작은 테스트를 추가한다. 중복을 제거하기 위해 리팩토링을 한다. (주기 1, 5)

--- src/test/java/fibonacci/FibonacciTest.java
+++ src/test/java/fibonacci/FibonacciTest.java
@@ -3,20 +3,20 @@ package fibonacci;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FibonacciTest {
 
     @Test
     void testFibonacci() {
-        assertEquals(fib(0), 0);
         assertEquals(fib(1), 1);
-        assertEquals(fib(2), 1);
-        assertEquals(fib(3), 2);
+        assertEquals(fib(4), 3);
+        assertThrows(IllegalArgumentException.class, () -> fib(-1));
     }
 
     private int fib(int n) {
-        if (n == 0) return 0;
-        if (n <= 2) return 1;
-        return 2;
+        if (n < 0) throw new IllegalArgumentException();
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
     }
 }
